f72a5e4c6c1709436f2b9b4c1e843d87aaeb4041
Nikolay Ovsyannikov <springmirror1@gmail.com>
2024-05-11 20:19:01 +1000

    here we go!

---
+++ python/task I-A - timer/timer.py
@@ -0,0 +1,47 @@
+##
+##File a.in :
+## HH:MM:SS 00<=HH<=23 
+## H:M:S , 00<=H,M,S<= 10^9
+## 01:01:01
+## 48:0:0
+##File a.out :
+## 01:01:01+2 days
+
+##unix timestamp way - convert time to a timestamp,
+##so we can easily add times to each other
+
+def normalise_time(a):
+    # 00:11:12
+    return normalised_time
+
+def parse_time(a):
+    pass
+
+
+def read_in(c):
+    
+    with open(c, "r") as input_file:
+        input_file.read()
+        input_time="some string"
+        counter_time="some other string"
+    output_time="00:00:00"
+    #debug
+    print(input_time)
+    print(counter_time)
+    #some computing
+    # 
+    # 
+    return output_time        
+
+read_in("C:\\Users\\Nittiyam\\Documents\\code\\olympiad-tasks\\python\\task I-A - timer\\a.in")
+
+def write_out(d):
+    with open(d, "w") as input_file:
+        input_file.write()
+
+
+output_time=read_in("./a.in")
+write_out("./a.out")
+
+
+
