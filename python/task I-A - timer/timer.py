5034f7d6472669d67b5d7382c37b513e3c77b896
Nikolay Ovsyannikov <springmirror1@gmail.com>
2024-05-12 17:45:52 +1000

    12.05.24

--- python/task I-A - timer/timer.py
+++ python/task I-A - timer/timer.py
@@ -10,18 +10,45 @@
 ##unix timestamp way - convert time to a timestamp,
 ##so we can easily add times to each other
 
-def normalise_time(a):
+def normalise_time(days, hours, minutes, seconds):
+    
+    # % - modulo остаток от деления
+    # // - floor division целая часть от деления
+    #seconds
+    floor_division = seconds // 60
+    seconds = seconds % 60
+    minutes = minutes + floor_division
+    #minutes
+    floor_division = minutes // 60
+    minutes = minutes % 60
+    hours = hours + floor_division
+    #hours
+    floor_division = hours // 60
+    hours = hours % 60
+    days = days + floor_division
+    normalised_time=(days,hours,minutes,seconds)
+    #seconds = seconds % 60
+    #minutes = minutes + (seconds // 60)
+    #hours = hours + (minutes // 60)
+    #minutes = minutes % 60
     # 00:11:12
     return normalised_time
 
-def parse_time(a):
-    pass
+def normalised_time_tuple(days,hours,minutes,seconds):
+    #idea is to iterate over the tuple without
+    #tuple variables name just by the numbers
+    #maybe it will take less memory
+    return normalised_time
 
 
-def read_in(c):
+def read_in(timetext):
     
-    with open(c, "r") as input_file:
+    with open(timetext, "r") as input_file:
         input_file.read()
+        #input_time=(days,hours,minutes,seconds)
+        #counter_time=(days,hours,minutes,seconds)
+        #   
+        #
         input_time="some string"
         counter_time="some other string"
     output_time="00:00:00"
@@ -31,15 +58,15 @@ def read_in(c):
     #some computing
     # 
     # 
-    return output_time        
+    return input_time, counter_time       
+
+def write_out(timetext, file):
+    with open(d, "w") as input_file:
+        input_file.write() 
+
+
 
 read_in("C:\\Users\\Nittiyam\\Documents\\code\\olympiad-tasks\\python\\task I-A - timer\\a.in")
-
-def write_out(d):
-    with open(d, "w") as input_file:
-        input_file.write()
-
-
 output_time=read_in("./a.in")
 write_out("./a.out")
 
